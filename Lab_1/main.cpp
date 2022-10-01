d3a7241ce1268cb29e9d6d05b6fbe674b4e7b363
Denys <denyspavlenko318@gmail.com>
2022-09-30 22:16:16 -0500

    foat: example

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -3,7 +3,25 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout <<"Hello"<< endl;
-cout<<"I realy like corn!"<<endl;
+    double x,y;
+    cout <<"Lab1"<< endl;
+cout<<"Enter the coordinates"<<endl;
+cout<<"x=";
+cin>>x;
+cout<<"y=";
+cin>>y;
+  if (x*x+y*y<=1 && 1>=x && x>=-1 && y<=1 && y >=0 || x*x+y*y<=1 && x>=-1 && x<=0 && y>=-1 && y<=0 ){
+    cout<<"The point is included"<<endl;}
+    else {
+        cout<<"The point is not included"<<endl;
+    }
+    while (!(cin >> y) || (cin.peek() != '\n'))
+    {
+        cin.clear();
+        while (cin.get() != '\n');
+        cout << "Error!" << endl;
+        system("pause"); // console pause
+    }
+
 }
 
