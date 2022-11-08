809ccdcfcfdb93b9ac053e5e1bb4b6eac1915360
Denys <denyspavlenko318@gmail.com>
2022-11-07 19:17:55 -0600

    foat: example

--- Lab_5/lab_5.cpp
+++ Lab_5/lab_5.cpp
@@ -10,9 +10,7 @@ using namespace std;
 int lab_5() {
     time(0);
     int size;
-    int *ar2;
     int *ar;
-    int j = 0;
     cout << "vvedit rozmir masuvy" << endl;
     cin >> size;
     ar = new int[size];
@@ -20,15 +18,19 @@ int lab_5() {
         ar[i] = rand() % 101 - 50;
         cout << ar[i] << "\t";
     }
-    for (int* l (ar), *r (ar + (size-1)); l < r; ) {
-        if (*l <= 0)
+    int* l = ar, *r = ar + (size-1);
+    for ( l , r ; l < r; ) {
+        if (*l < 0)
             ++l;
-        else if (*r <= 0)
+        else if (*r < 0)
             --r;
         else
             swap(*l++, *r--);
     }
+
     cout<<endl;
+    cout<<"vuhidnyu masuv"<<endl;
+
     for (int i = 0; i < size; ++i)
          cout << ar[i] << "\t";
         delete[] ar;
